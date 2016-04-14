a840d0c9800100d51d1768ecfb527f8e1c827462
Insuasti Juan <locke189@hotmail.com>
2016-04-14 21:07:01 +0000

    Complete Cleanup

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,9 +1,8 @@
 sap.ui.define([
 		"sap/ui/core/UIComponent",
 		"sap/ui/Device",
-		"demo_app/com/model/models",
-		"demo_app/com/controller/ErrorHandler"
-	], function (UIComponent, Device, models, ErrorHandler) {
+		"demo_app/com/model/models"
+	], function (UIComponent, Device, models) {
 		"use strict";
 
 		return UIComponent.extend("demo_app.com.Component", {
@@ -22,26 +21,11 @@ sap.ui.define([
 				// call the base component's init function
 				UIComponent.prototype.init.apply(this, arguments);
 
-				// initialize the error handler with the component
-				this._oErrorHandler = new ErrorHandler(this);
-
 				// set the device model
 				this.setModel(models.createDeviceModel(), "device");
 
 				// create the views based on the url/hash
 				this.getRouter().initialize();
-			},
-
-			/**
-			 * The component is destroyed by UI5 automatically.
-			 * In this method, the ErrorHandler is destroyed.
-			 * @public
-			 * @override
-			 */
-			destroy : function () {
-				this._oErrorHandler.destroy();
-				// call the base component's destroy function
-				UIComponent.prototype.destroy.apply(this, arguments);
 			},
 
 			/**
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,33 +1,11 @@
 sap.ui.define([
-		"demo_app/com/controller/BaseController",
-		"sap/ui/model/json/JSONModel"
-	], function (BaseController, JSONModel) {
+		"demo_app/com/controller/BaseController"
+	], function (BaseController) {
 		"use strict";
 
 		return BaseController.extend("demo_app.com.controller.App", {
 
-			onInit : function () {
-				var oViewModel,
-					fnSetAppNotBusy,
-					iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
-
-				oViewModel = new JSONModel({
-					busy : true,
-					delay : 0
-				});
-				this.setModel(oViewModel, "appView");
-
-				fnSetAppNotBusy = function() {
-					oViewModel.setProperty("/busy", false);
-					oViewModel.setProperty("/delay", iOriginalBusyDelay);
-				};
-
-				this.getOwnerComponent().getModel().metadataLoaded().
-					then(fnSetAppNotBusy);
-
-				// apply content density mode to root view
-				this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
-			}
+	
 		});
 
 	}
